c3cb16d03ae50bb829940cd9eb78bdf8b642f56d
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-16 16:49:25 +0100

    Add test code for receipt in program.cs

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exercise.main;
+
+var _customerBasket = new CustomerBasket();
+
+_customerBasket.AddItem("BGLO" , 0.49f , "Bagel" , "Onion");
+_customerBasket.AddItem("COFB" , 0.99f , "Coffee" , "Black");
+_customerBasket.AddSpecialOffer("COFB" , 1.25f);
+
+var receipt = new Receipt(_customerBasket);
+receipt.PrintReceipt();
