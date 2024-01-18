e69db47ce2a3d8a6dece460fd64a876a9643d506
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-18 18:00:16 +0100

    Add new class for handling Sms notification

---
+++ exercise.main/SMSNotifier.cs
@@ -0,0 +1,31 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace exercise.main
+{
+    public class SMSNotifier
+    {
+        private string accountSid;
+        private string authToken;
+        private string fromPhoneNumber;
+
+        public SMSNotifier(string accountSid , string authToken , string fromPhoneNumber)
+        {
+            this.accountSid = accountSid;
+            this.authToken = authToken;
+            this.fromPhoneNumber = fromPhoneNumber;
+        }
+
+        public virtual void SendSMS(string toPhoneNumber , string message)
+        {
+            TwilioClient.Init(accountSid , authToken);
+
+            var messageOptions = new CreateMessageOptions(
+                new Twilio.Types.PhoneNumber(toPhoneNumber));
+            messageOptions.From = new Twilio.Types.PhoneNumber(fromPhoneNumber);
+            messageOptions.Body = message;
+
+            var messageResource = MessageResource.Create(messageOptions);
+        }
+    }
+}
