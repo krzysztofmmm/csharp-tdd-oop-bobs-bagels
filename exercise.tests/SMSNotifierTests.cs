326230f7959e720385dc91c7121813342f6fa358
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-18 17:59:38 +0100

    Add new Test tio check if this even work

---
+++ exercise.tests/SMSNotifierTests.cs
@@ -0,0 +1,25 @@
+﻿using exercise.main;
+using Moq;
+
+namespace exercise.tests
+{
+    [TestFixture]
+    public class SMSNotifierTests
+    {
+        private Mock<SMSNotifier> _smsNotifierMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _smsNotifierMock = new Mock<SMSNotifier>("accountSid" , "authToken" , "fromPhoneNumber");
+        }
+
+        [Test]
+        public void TestSendSMS()
+        {
+            _smsNotifierMock.Setup(m => m.SendSMS(It.IsAny<string>() , It.IsAny<string>()));
+            _smsNotifierMock.Object.SendSMS("toPhoneNumber" , "message");
+            _smsNotifierMock.Verify(m => m.SendSMS(It.IsAny<string>() , It.IsAny<string>()) , Times.Once);
+        }
+    }
+}
